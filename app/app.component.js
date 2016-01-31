b4e8eebc8b739450d4c26563b5cf7ec87663e991
pshin <iuliiap@ya.ru>
2016-01-31 21:01:35 +0200

    Added one type of slider

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
     app.AppComponent =
         ng.core.Component({
                 selector: 'my-app',
-                template: '<h1>My First Angular 2 App</h1>'
+                template: '<h1>1. Choose type of your slider</h1>'
             })
             .Class({
                 constructor: function () {
